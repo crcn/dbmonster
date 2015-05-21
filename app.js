5f809ce10ddf5a7eb85fcaeca99ee24c1de7f661
Craig Condon <craig.condon@hellosign.com>
2015-05-20 18:03:32 -0700

    update to paperclip@3.x

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ function getData() {
     start_at: new Date().getTime() / 1000,
     databases: {}
   };
-  
+
   for (var i = 1; i <= ENV.rows; i++) {
     data.databases["cluster" + i] = {
       queries: []
     };
-    
+
     data.databases["cluster" + i + "slave"] = {
       queries: []
     };
@@ -34,10 +34,10 @@ function getData() {
   Object.keys(data.databases).forEach(function(dbname) {
     var info = data.databases[dbname];
 
-    
+
     var r = Math.floor((Math.random() * 10) + 1);
     for (var i = 0; i < r; i++) {
-      var q = { 
+      var q = {
         canvas_action: null,
         canvas_context_id: null,
         canvas_controller: null,
@@ -48,15 +48,15 @@ function getData() {
         query: "SELECT blah FROM something",
         waiting: Math.random() < 0.5
       };
-      
+
       if (Math.random() < 0.2) {
         q.query = "<IDLE> in transaction";
       }
-      
+
       if (Math.random() < 0.1) {
         q.query = "vacuum";
       }
-      
+
       info.queries.push(q);
     }
 
@@ -73,7 +73,7 @@ function getData() {
 	});
 
 	if (samples.length > 5) {
-	  samples.splice(0, samples.length - 5);      
+	  samples.splice(0, samples.length - 5);
 	}
 
 	var db = {
@@ -88,7 +88,7 @@ function getData() {
   });
 
 
-  
+
   return data2;
 }
 
